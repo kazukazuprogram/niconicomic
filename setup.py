31541f84ce3e0a5bf6c288c0b4434236a2cca91e
kazukazuprogram <33714346+kazukazuprogram@users.noreply.github.com>
2020-10-01 22:28:22 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="niconicomic",
+    version="0.0.0",
+    description="Niconico Comic's downloader.",
+    license="MIT",
+    author="kazukazuprogram",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+    ]
+)
---
+++ src/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from requests import Session
+from bs4 import BeautifulSoup as bs
+from sys import argv
+
+def main(args=argv, s=Session()):
+    pass
+
+if __name__ == '__main__':
+    main()
